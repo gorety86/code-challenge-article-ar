52475a967edea0d4e5fc947a92ad7250babb6ee5
gorety <wagutugorety@gmail.com>
2023-05-26 23:28:35 +0300

    author

--- lib/Article.rb
+++ lib/Article.rb
@@ -1,15 +1,15 @@
 class Article
    attr_reader :author, :magazine, :title
-   @@all_articles = []
+   @@articles = []
 
    def initialize(author, magazine, title)
      @author = author
      @magazine = magazine
      @title = title
-     @@all_articles << self
+     @@articles << self
    end
 
    def self.all
-    @@all_articles
+    @@articles
    end
 end
