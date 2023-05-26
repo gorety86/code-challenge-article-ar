0234bca8f51172405d82a681b4b95c9cd48dfb09
gorety <wagutugorety@gmail.com>
2023-05-26 18:39:53 +0300

    article

--- lib/Article.rb
+++ lib/Article.rb
@@ -1,4 +1,9 @@
 class Article
    attr_reader :author, :magazine, :title
-    
-    end
+
+   def initialize(author, magazine, title)
+     @author = author
+     @magazine = magazine
+     @title = title
+   end
+end
