ad50b33f9cf6452d4b6ba109ac6cdd6c26d61bc7
gorety <wagutugorety@gmail.com>
2023-05-26 18:33:44 +0300

    author

---
+++ lib/Author.rb
@@ -0,0 +1,13 @@
+class Author
+    attr_reader :name
+        def initialize(name)
+            @name = name
+        end
+    
+        def name
+            @name
+        end
+    end
+    lorena = Author.new("keith")
+    lorena.name
+    
